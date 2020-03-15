6a53a5bfc1bef23527ccfc149f69a101b4533255
Mohammad,Ashif <ashif.mohemmad@gmail.com>
2020-03-15 15:39:07 +0530

    Unit tests for Services.js #15

---
+++ src/services/__tests__/service.test.js
@@ -0,0 +1,26 @@
+
+const AuthService = require('../auth-service')
+describe('AuthService.js >> auth service should validate',()=>{
+
+    test('whether AuthService is accessible',()=>{
+        expect(AuthService).toBeTruthy()
+        const { login } = AuthService
+        console.log(login)
+    })
+
+    test('whether AuthService has login function',()=>{
+        const { login } = AuthService
+
+        expect(login).toBeInstanceOf(Function)
+    })
+    test('whether login function returns result with no args',()=>{
+        const { login } = AuthService
+        expect(login()).toBe(true)
+
+    })
+    test('whether login function returns with args',()=>{
+        const { login } = AuthService
+        expect(login("testUser", "$666@12")).toBe("testUser")
+
+    })
+})
