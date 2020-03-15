20170af1838e2f6d28789e100cab30180b44e9a3
Abdul Wahed <abdul.wahed11314@gmail.com>
2020-03-15 17:17:14 +0530

    added changes for lint

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ const authRouter = require('./routes/auth-router');
 
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false })  );
 app.use(bodyParser.json());
 
 // CORS
-app.use(function (req, res, next) {
+app.use(function (req, res, next  ) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -17,10 +17,9 @@ app.use(function (req, res, next) {
 });
 
 // Register routers
-app.use('/', defaultRouter);
-app.use('/api/auth', authRouter);
+app.use('/', defaultRouter  );
+app.use('/api/auth',   authRouter);
 
 // Run
-app.listen(PORT, () => {
-   console.log(`Authentication service up on port ${PORT}`);
+app.listen(PORT, () => { console.log(`Authentication service up on port ${PORT}`);
 });
